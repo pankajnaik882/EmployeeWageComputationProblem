cf29d023b283e695189378f8ba609f252fb3718d
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-26 21:34:37 +0530

    Refector : Solving using Switch case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,20 @@
 echo "Welcome to Employee Wage Computation Program"
 
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+isPartTime=1
+isFullTime=2
+empRatePerHrs=20
 
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
+randomCheck=$((RANDOM%3))
 
-salary=$(($empHrs*$empRatePerHr))
+case $randomCheck in $isFullTime)
+			empHrs=8;;
+		     $isPartTime)
+			empHrs=4;;
+		     *)
+		     $empHrs=0;;
+esac
+
+salary=$(($empHrs*$empRatePerHrs))
 echo "salary=$salary"
 
 
