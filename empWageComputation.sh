223b34fc2bdd06a8afc68c9b2c6a4501432daa27
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-26 22:32:55 +0530

    Add: Calculate Wages till a condition of total working hours or days is reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,27 +2,42 @@ echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1
 isFullTime=2
-empRatePerHrs=20
+empHrs=0
 totalSalary=0
+totalWorkingHour=0;
+empRatePerHr=20
 numWorkingDays=20
-
-for (( day=1;day<=$numWorkingDays; day++ ))
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
-	empCheck=$((RANDOM%3))
-	case $empCheck in $isFullTime)
-			 empHrs=8;;
-		         $isPartTime)
-			 empHrs=4;;
-		        *)
-		        empHrs=0;;
+
+randomCheck=$((RANDOM%3));
+
+case $randomCheck in $isFullTime)
+		     empHrs=8;;
+		     $isPartTime)
+		     empHrs=4;;
+		     *)
+		     empHrs=0;;
 esac
 
-salary=$(($empHrs*$empRatePerHrs))
-totalSalary=$(($totalSalary+$salary))
+totalWorkingHour=$(($totalWorkingHour+$empHrs))
 
+if [ $totalWorkingHour -gt 100 ]
+then
+totalWorkingHour=$(($totalWorkingHour - $empHrs));
+break
+fi
+salary=$(($empRatePerHr*$empHrs))
+totalSalary=$(($totalSalary+$salary))
+((day++))
 done
 
-echo "Employee has earned $totalSalary in a month"
+echo "Employee has earned $totalSalary $ in a month (Total Working Hour : $totalWorkingHour)"
+
+
+
+
 
 
 
